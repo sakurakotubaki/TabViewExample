9a7eb47ea804a618c0bc17ecdadaad7f6e667de1
sakurakotubaki <gitonly543@gmail.com>
2023-12-15 06:32:00 +0900

    Initial Commit

---
+++ LunchApp/LunchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchAppApp.swift
+//  LunchApp
+//
+//  Created by 橋本純一 on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct LunchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
